f10626f5a6b685e2f1e7c2ac3d985815fb99e21e
Kevin Xu Ke <lkstc112233@gmail.com>
2015-02-16 22:28:28 +0800

    Project created
    A project creates XML for the game.

---
+++ ProjectTitan/ProjectTitan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectTitan
+//
+//  Created by 煦 柯 on 15/2/16.
+//  Copyright (c) 2015年 Must Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
